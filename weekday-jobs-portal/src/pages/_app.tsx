b75e3d639139a43d4d9e5acc817a3fc157e629c5
KUNTIMADDI MANIDEEP <manideepk90@gmail.com>
2024-05-03 11:24:58 +0530

    FEATURE/ADDED_MUI_THEME

--- weekday-jobs-portal/src/pages/_app.tsx
+++ weekday-jobs-portal/src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 import { AppCacheProvider } from "@mui/material-nextjs/v13-pagesRouter";
 import type { AppProps } from "next/app";
-
+import theme from "@/utils/theme";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppCacheProvider {...pageProps}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </AppCacheProvider>
   );
 }
